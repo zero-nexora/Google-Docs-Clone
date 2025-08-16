6abbb419cdfa887d6e2f9ba19ad1a912638996d1
Nguyen Hien <hienkoi05@gmail.com>
2025-08-16 11:34:33 +0700

    continue

--- src/app/documents/[documentId]/document.tsx
+++ src/app/documents/[documentId]/document.tsx
@@ -7,6 +7,7 @@ import { Editor } from "./editor";
 import { Navbar } from "./navbar";
 import { Toolbar } from "./toolbar";
 import { api } from "../../../../convex/_generated/api";
+import { cn } from "@/lib/utils";
 
 interface DocumentProps {
   preloadedDocument: Preloaded<typeof api.documents.getById>;
@@ -18,7 +19,11 @@ export const Document = ({ preloadedDocument }: DocumentProps) => {
   return (
     <Room>
       <div className="min-h-screen bg-editor-bg">
-        <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden h-[112px]">
+        <div
+          className={cn(
+            "flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden h-[112px]"
+          )}
+        >
           <Navbar data={document} />
           <Toolbar />
         </div>
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -81,7 +81,8 @@ const LineHeightButton = () => {
             onClick={() => editor?.chain().focus().setLineHeight(value).run()}
             className={cn(
               "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
-              editor?.getAttributes("paragraph").lineHeight === value && "bg-neutral-200/80"
+              editor?.getAttributes("paragraph").lineHeight === value &&
+                "bg-neutral-200/80"
             )}
           >
             <span className="text-sm">{label}</span>
@@ -322,7 +323,10 @@ const ImageButton = () => {
             <UploadIcon className="size-4 mr-2" />
             Upload
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setIsDialogOpen(true)} className="cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => setIsDialogOpen(true)}
+            className="cursor-pointer"
+          >
             <SearchIcon className="size-4 mr-2" />
             Paste image url
           </DropdownMenuItem>
@@ -370,7 +374,10 @@ const LinkButton = () => {
         }
       }}
     >
-      <DropdownMenuTrigger onClick={() => setValue(editor?.getAttributes("link").href)} asChild>
+      <DropdownMenuTrigger
+        onClick={() => setValue(editor?.getAttributes("link").href)}
+        asChild
+      >
         <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
           <Link2Icon className="size-4" />
         </button>
@@ -424,7 +431,10 @@ const TextColorButton = () => {
       <DropdownMenuTrigger asChild>
         <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
           <span className="text-xs">A</span>
-          <div className="h-0.5 w-full" style={{ backgroundColor: value }}></div>
+          <div
+            className="h-0.5 w-full"
+            style={{ backgroundColor: value }}
+          ></div>
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="p-0 border-0">
@@ -439,11 +449,11 @@ const HeadingLevelButton = () => {
 
   const headings = [
     { label: "Normal text", value: 0, fontSize: "16px" },
-    { label: "Heding 1", value: 1, fontSize: "32px" },
-    { label: "Heding 2", value: 2, fontSize: "24px" },
-    { label: "Heding 3", value: 3, fontSize: "20px" },
-    { label: "Heding 4", value: 4, fontSize: "18px" },
-    { label: "Heding 5", value: 5, fontSize: "16px" },
+    { label: "Heading 1", value: 1, fontSize: "32px" },
+    { label: "Heading 2", value: 2, fontSize: "24px" },
+    { label: "Heading 3", value: 3, fontSize: "20px" },
+    { label: "Heading 4", value: 4, fontSize: "18px" },
+    { label: "Heading 5", value: 5, fontSize: "16px" },
   ];
 
   const getCurrentHeading = () => {
@@ -459,7 +469,7 @@ const HeadingLevelButton = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="h-7 min-w-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+        <button className="h-7 shrink-0 flex items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
           <span className="truncate">{getCurrentHeading()}</span>
           <ChevronDownIcon className="ml-2 size-4 shrink-0" />
         </button>
@@ -472,7 +482,8 @@ const HeadingLevelButton = () => {
             className={cn(
               "flex items-center gap-x-2 px-2 py-1 font-[value] rounded-sm hover:bg-neutral-200/80",
               (value === 0 && !editor?.isActive("heading")) ||
-                (editor?.isActive("heading", { level: value as Level }) && "bg-neutral-200/80")
+                (editor?.isActive("heading", { level: value as Level }) &&
+                  "bg-neutral-200/80")
             )}
             onClick={() => {
               if (value === 0) {
@@ -522,7 +533,8 @@ export const FontFamilyButton = () => {
             key={value}
             className={cn(
               "flex items-center gap-x-2 px-2 py-1 font-[value] rounded-sm hover:bg-neutral-200/80",
-              editor?.getAttributes("textStyle").fontFamily === value && "bg-neutral-200/80"
+              editor?.getAttributes("textStyle").fontFamily === value &&
+                "bg-neutral-200/80"
             )}
             style={{ fontFamily: value }}
           >
@@ -540,7 +552,11 @@ interface ToolbarButtonProps {
   icon: LucideIcon;
 }
 
-const ToolbarButton = ({ onClick, isActive, icon: Icon }: ToolbarButtonProps) => {
+const ToolbarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: ToolbarButtonProps) => {
   return (
     <button
       onClick={onClick}
@@ -557,76 +573,83 @@ const ToolbarButton = ({ onClick, isActive, icon: Icon }: ToolbarButtonProps) =>
 export const Toolbar = () => {
   const { editor } = useEditorStore();
 
-  const sections: { label: string; icon: LucideIcon; onClick: () => void; isActive?: boolean }[][] =
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
     [
-      [
-        {
-          label: "Undo",
-          icon: Undo2Icon,
-          isActive: editor?.can().undo(),
-          onClick: () => editor?.chain().focus().undo().run(),
+      {
+        label: "Undo",
+        icon: Undo2Icon,
+        isActive: editor?.can().undo(),
+        onClick: () => editor?.chain().focus().undo().run(),
+      },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        isActive: editor?.can().redo(),
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: "Spell Check",
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "true" : "false"
+          );
         },
-        {
-          label: "Redo",
-          icon: Redo2Icon,
-          isActive: editor?.can().redo(),
-          onClick: () => editor?.chain().focus().redo().run(),
-        },
-        {
-          label: "Print",
-          icon: PrinterIcon,
-          onClick: () => window.print(),
-        },
-        {
-          label: "Spell Check",
-          icon: SpellCheckIcon,
-          onClick: () => {
-            const current = editor?.view.dom.getAttribute("spellcheck");
-            editor?.view.dom.setAttribute("spellcheck", current === "false" ? "true" : "false");
-          },
-        },
-      ],
-      [
-        {
-          label: "Bold",
-          icon: BoldIcon,
-          isActive: editor?.isActive("bold"),
-          onClick: () => editor?.chain().focus().toggleBold().run(),
-        },
-        {
-          label: "Italic",
-          icon: ItalicIcon,
-          isActive: editor?.isActive("italic"),
-          onClick: () => editor?.chain().focus().toggleItalic().run(),
-        },
-        {
-          label: "Underline",
-          icon: UnderlineIcon,
-          isActive: editor?.isActive("underline"),
-          onClick: () => editor?.chain().focus().toggleUnderline().run(),
-        },
-      ],
-      [
-        {
-          label: "Comment",
-          icon: MessageSquarePlusIcon,
-          onClick: () => editor?.chain().focus().addPendingComment().run(),
-          isActive: editor?.isActive("liveblocksCommentMark"), 
-        },
-        {
-          label: "List Todo",
-          icon: ListTodoIcon,
-          onClick: () => editor?.chain().focus().toggleTaskList().run(),
-          isActive: editor?.isActive("taskList"),
-        },
-        {
-          label: "Remove Formatting",
-          icon: RemoveFormattingIcon,
-          onClick: () => editor?.chain().focus().unsetAllMarks().run(),
-          isActive: editor?.isActive("taskList"),
-        },
-      ],
-    ];
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        isActive: editor?.isActive("bold"),
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
+      {
+        label: "Italic",
+        icon: ItalicIcon,
+        isActive: editor?.isActive("italic"),
+        onClick: () => editor?.chain().focus().toggleItalic().run(),
+      },
+      {
+        label: "Underline",
+        icon: UnderlineIcon,
+        isActive: editor?.isActive("underline"),
+        onClick: () => editor?.chain().focus().toggleUnderline().run(),
+      },
+    ],
+    [
+      {
+        label: "Comment",
+        icon: MessageSquarePlusIcon,
+        onClick: () => editor?.chain().focus().addPendingComment().run(),
+        isActive: editor?.isActive("liveblocksCommentMark"),
+      },
+      {
+        label: "List Todo",
+        icon: ListTodoIcon,
+        onClick: () => editor?.chain().focus().toggleTaskList().run(),
+        isActive: editor?.isActive("taskList"),
+      },
+      {
+        label: "Remove Formatting",
+        icon: RemoveFormattingIcon,
+        onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+        isActive: editor?.isActive("taskList"),
+      },
+    ],
+  ];
 
   return (
     <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
@@ -636,7 +659,9 @@ export const Toolbar = () => {
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       <FontFamilyButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
-      <HeadingLevelButton />
+      <div className="min-w-28 max-w-28">
+        <HeadingLevelButton />
+      </div>
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       <FontSizeButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
